341c8aa9d2b048736927a8019223c1b99d3ea047
shinjjune <sjh8236@naver.com>
2019-11-21 16:47:42 +0900

    upupupu

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 });
 app.use(auth.initialize());
 app.use(express.json());
+app.use("/auth", user);
 app.use("/stamp", user);
 app.use("/advertise", advertise);
 app.use(() => mongoose.disconnect());
