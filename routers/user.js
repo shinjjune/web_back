f10d90650f767635ee50dc60f063440b08e34012
shinjjune <sjh8236@naver.com>
2019-11-26 19:57:52 +0900

    pass

--- routers/user.js
+++ routers/user.js
@@ -138,7 +138,7 @@ router.post(
     await User.updateMany(
       { id: req.user.id },
       {
-        password: req.body.password,
+        password: req.body.newPassword,
         company_name: req.body.company_name,
         company_location: req.body.company_location,
         phonenumber: req.body.phonenumber
